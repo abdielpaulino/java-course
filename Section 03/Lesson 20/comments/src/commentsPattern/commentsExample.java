a37282717739efc9eb169b0d2f42e68c68987c64
Abdiel Paulino <164289941+abdielpaulino@users.noreply.github.com>
2026-09-20 23:13:28 -0300

    Add files via upload

---
+++ Section 03/Lesson 20/comments/src/commentsPattern/commentsExample.java
@@ -0,0 +1,33 @@
+package commentsPattern;
+
+/**
+ * This class represents - JAVADOC
+ * 
+ * @author Abdiel Paulino <@email.com>
+ * @since JDK1.0
+ * @see (other links)
+ */
+
+public class commentsExample {
+	
+	/**
+	 * 
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		
+		// A code statement ends with a semicolon (;).
+		// More comments here...
+		// End
+		System.out.println("Hello World!");
+		
+		/*
+		 * Line one;
+		 * Line two;
+		 * Line three
+		 */
+		
+		System.out.println("Goodbye World!"); // Here work too.
+	}
+}
